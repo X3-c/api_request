7485a0ec5575c754c51770c9b9124f675d048999
Delo <szabo.levente.626@gmail.com>
2023-02-18 13:35:46 +0100

    Search

--- lib/home.dart
+++ lib/home.dart
@@ -19,9 +19,22 @@ Future<List<Users>> fetchUser(http.Client client) async {
 class Users {
   final String email;
   final String name;
-  const Users({required this.email, required this.name});
+  final String username;
+  final String phone;
+  final String website;
+  const Users(
+      {required this.email,
+      required this.name,
+      required this.username,
+      required this.phone,
+      required this.website});
   factory Users.fromJson(Map<String, dynamic> json) {
-    return Users(email: json['email'], name: json['name']);
+    return Users(
+        email: json['email'],
+        name: json['name'],
+        username: json['username'],
+        phone: json['phone'],
+        website: json['website']);
   }
 }
 
@@ -42,33 +55,91 @@ class _HomeScreenState extends State<HomeScreen> {
     futureUser = fetchUser(http.Client());
   }
 
+  var searchString = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: FutureBuilder<List<Users>>(
-          future: futureUser,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return SingleChildScrollView(
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: snapshot.data?.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(snapshot.data![index].name),
-                      subtitle: Text(snapshot.data![index].email),
-                    );
-                  },
-                ),
-              );
-            } else if (snapshot.hasError) {
-              return const Text("Error");
-            } else if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Text("Getting Data...");
-            }
-            return const CircularProgressIndicator();
-          },
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              onChanged: (value) {
+                setState(() {
+                  searchString = value.toLowerCase();
+                });
+              },
+              decoration: InputDecoration(
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  prefixIcon: const Icon(Icons.search)),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Expanded(
+                child: FutureBuilder<List<Users>>(
+              future: futureUser,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return SingleChildScrollView(
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: snapshot.data?.length,
+                      itemBuilder: (context, index) {
+                        return snapshot.data![index].name
+                                .toLowerCase()
+                                .contains(searchString)
+                            ? Card(
+                                elevation: 4,
+                                child: ExpansionTile(
+                                  expandedCrossAxisAlignment:
+                                      CrossAxisAlignment.end,
+                                  title: Text(
+                                    snapshot.data![index].name,
+                                    style: const TextStyle(
+                                        fontSize: 16.0,
+                                        fontWeight: FontWeight.w500),
+                                  ),
+                                  subtitle: Text(snapshot.data![index].email),
+                                  children: [
+                                    ListTile(
+                                      title: Text(
+                                        'Username: ${snapshot.data![index].username}',
+                                      ),
+                                    ),
+                                    ListTile(
+                                      title: Text(
+                                        'Website: ${snapshot.data![index].website}',
+                                      ),
+                                    ),
+                                    ListTile(
+                                      title: Text(
+                                        'Phone: ${snapshot.data![index].phone}',
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              )
+                            : Container();
+                      },
+                    ),
+                  );
+                } else if (snapshot.hasError) {
+                  return const Text("Error");
+                } else if (snapshot.connectionState ==
+                    ConnectionState.waiting) {
+                  return const Text("Getting Data..");
+                }
+                return const CircularProgressIndicator();
+              },
+            ))
+          ],
         ),
       ),
     );
